9425a9b35c01ddf513437dabbf5a3b81c8468fc7
diegoyus <135970094+diegoyus@users.noreply.github.com>
2025-08-17 04:42:13 -0700

    Fix import path for supabaseClient in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import { useRouter } from 'next/router';
 
 /**
